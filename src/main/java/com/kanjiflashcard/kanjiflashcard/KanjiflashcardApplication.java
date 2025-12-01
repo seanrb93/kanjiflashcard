6ea59bae9c681bdc15593d31bd1bd66afc9fd5a6
Sean Brankin <SeanBrankin@mail.com>
2025-12-01 15:25:41 +0000

    Initial commit

---
+++ src/main/java/com/kanjiflashcard/kanjiflashcard/KanjiflashcardApplication.java
@@ -0,0 +1,13 @@
+package com.kanjiflashcard.kanjiflashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjiflashcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanjiflashcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanjiflashcard/kanjiflashcard/KanjiflashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanjiflashcard.kanjiflashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanjiflashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
