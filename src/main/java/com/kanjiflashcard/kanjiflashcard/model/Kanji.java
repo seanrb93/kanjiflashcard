5c93a906fb2b2316df73a6e7f8e36845023be46d
Sean Brankin <SeanBrankin@mail.com>
2025-12-01 18:29:44 +0000

    added lombok to pom and added getters and setters to Kanji object

--- src/main/java/com/kanjiflashcard/kanjiflashcard/model/Kanji.java
+++ src/main/java/com/kanjiflashcard/kanjiflashcard/model/Kanji.java
@@ -1,16 +1,28 @@
 package com.kanjiflashcard.kanjiflashcard.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class Kanji {
+
     @Id
+    @Getter @Setter
     private String character;
+
+    @Getter @Setter
     private String meaning;
+
+    @Getter @Setter
     private String onyomi;
+
+    @Getter @Setter
     private String kunyomi;
+
+    @Getter @Setter
     private int strokeCount;
+    
 }
 
