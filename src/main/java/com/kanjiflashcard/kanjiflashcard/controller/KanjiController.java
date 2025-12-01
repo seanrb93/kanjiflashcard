3ac332ffa1df15b2291ee47e2cd3778436f1393b
Sean Brankin <SeanBrankin@mail.com>
2025-12-01 20:24:33 +0000

    Add random kanji endpoint

--- src/main/java/com/kanjiflashcard/kanjiflashcard/controller/KanjiController.java
+++ src/main/java/com/kanjiflashcard/kanjiflashcard/controller/KanjiController.java
@@ -4,20 +4,31 @@ import java.util.List;
 import com.kanjiflashcard.kanjiflashcard.model.Kanji;
 import com.kanjiflashcard.kanjiflashcard.repository.KanjiRepository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.PageRequest;
 
 @RestController
 @RequestMapping("/api")
 public class KanjiController {
 
-    private final KanjiRepository kanjiRepository;
+    private final KanjiRepository kanjiRepo;
 
     public KanjiController(KanjiRepository kanjiRepository) {
-        this.kanjiRepository = kanjiRepository;
+        this.kanjiRepo = kanjiRepository;
     }
 
     @GetMapping("/kanji")
     public List<Kanji> getAllKanji() {
-        return kanjiRepository.findAll();
+        return kanjiRepo.findAll();
+    }
+
+    @GetMapping("/random")
+    public Kanji getRandomKanji() {
+        long count = kanjiRepo.count();
+        if (count == 0) {
+            return null; // Or throw an exception
+        }
+        int randomIndex = (int) (Math.random() * count);
+        return kanjiRepo.findAll(PageRequest.of(randomIndex, 1)).getContent().get(0);
     }
 
 }
