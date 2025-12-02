3b94dd2e5c264d41200d3c549b66214d6cfc94eb
Sean Brankin <SeanBrankin@mail.com>
2025-12-02 12:12:35 +0000

    Removed unused import

--- src/main/java/com/kanjiflashcard/kanjiflashcard/controller/KanjiController.java
+++ src/main/java/com/kanjiflashcard/kanjiflashcard/controller/KanjiController.java
@@ -1,6 +1,5 @@
 package com.kanjiflashcard.kanjiflashcard.controller;
 
-import java.util.List;
 import com.kanjiflashcard.kanjiflashcard.model.Kanji;
 import com.kanjiflashcard.kanjiflashcard.repository.KanjiRepository;
 import org.springframework.web.bind.annotation.*;
