52554bf43ff2fb0c9470cd9aeafdbd3d63719fac
Sean Brankin <SeanBrankin@mail.com>
2025-12-02 14:41:53 +0000

    Add JLPT level logic to random endpoint, added jlpt level to kanji repository, added jlpt buttons to flashcard page

--- src/main/java/com/kanjiflashcard/kanjiflashcard/repository/KanjiRepository.java
+++ src/main/java/com/kanjiflashcard/kanjiflashcard/repository/KanjiRepository.java
@@ -3,7 +3,10 @@ package com.kanjiflashcard.kanjiflashcard.repository;
 
 import com.kanjiflashcard.kanjiflashcard.model.Kanji;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface KanjiRepository extends JpaRepository<Kanji, String> {
+
+    List<Kanji> findByJlptLevel(Integer jlptLevel);
 
 }
