53fd4529ec068e0fe9d74ee509f53f94904d6a91
Sean Brankin <SeanBrankin@mail.com>
2025-12-04 15:29:17 +0000

    Changed name of statefile

--- terraform/aws/app/backend.tf
+++ terraform/aws/app/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "kanji-tf-state-srb"
-    key            = "aws/terraform.tfstate"
+    key            = "aws/app.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
